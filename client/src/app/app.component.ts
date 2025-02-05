d768588722baa6dc0caea5d7f823c03b1a60094b
Yassine Dergaoui <bcha2825@hotmail.fr>
2025-02-05 08:29:20 +0100

    Added http sample code in client

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component, inject, OnInit} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    imports: [RouterOutlet],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'client';
+export class AppComponent implements OnInit {
+    http = inject(HttpClient);
+    api = 'https://localhost:5001/api/users';
+    users :any;
+
+    ngOnInit() {
+        this.http.get(this.api).subscribe({
+            next: data => this.users = data,
+            error: error => console.error(error),
+            complete: () => console.log('completed fetching users')
+        });
+    }
+
 }
