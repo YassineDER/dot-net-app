7fc1dd3e908aa7ef3d5d116a8d5ec0fe98ba1507
Yassine Dergaoui <bcha2825@hotmail.fr>
2025-02-16 11:30:24 +0100

    improved login in frontend (with logout), added navbar

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,24 +1,37 @@
 import {Component, inject, OnInit} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
-import {HttpClient} from "@angular/common/http";
+import {NavComponent} from "./nav/nav.component";
+import {AccountService} from "./_services/account.service";
+import {User} from "./_models/user";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet],
+    imports: [RouterOutlet, NavComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-    http = inject(HttpClient);
-    api = 'https://localhost:5001/api/users';
+    private accountService = inject(AccountService);
     users :any;
 
     ngOnInit() {
-        this.http.get(this.api).subscribe({
-            next: data => this.users = data,
-            error: error => console.error(error),
-            complete: () => console.log('completed fetching users')
+        this.getUsers();
+        this.setCurrentUser();
+    }
+
+    setCurrentUser() {
+        const userString = localStorage.getItem('user');
+        if (!userString) return;
+        const user = JSON.parse(userString) as User;
+        this.accountService.currentUser.set(user);
+    }
+
+
+    getUsers() {
+        this.accountService.getUsers().subscribe({
+            next: users => this.users = users,
+            error: error => console.log(error)
         });
     }
 
